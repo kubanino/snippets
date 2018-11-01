afc664ab3b3342044d682111028f1ad60d375f96
Jakub Dubański <jakub.dubanski@gmail.com>
2018-11-01 22:55:43 +0100

    [Snippets] Static factory method.

---
+++ src/main/java/com/public_class/snippets/staticFactoryMethod/Car.java
@@ -0,0 +1,72 @@
+package com.public_class.snippets.staticFactoryMethod;
+
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Car
+{
+    private static final String OPEL = "OPEL";
+    private static final String BMW = "BMW";
+
+    private String name;
+
+    private static final Map<String, Car> carsCache = new HashMap<>();
+
+    static
+    {
+        carsCache.put(BMW, new Car(BMW));
+        carsCache.put(OPEL, new Car(OPEL));
+    }
+
+    private Car(String name)
+    {
+        this.name = name;
+    }
+
+    public static Car of(final String brand)
+    {
+        final Car instance = carsCache.get(brand.toUpperCase());
+
+        if (instance == null)
+        {
+            throw new IllegalArgumentException("Wrong brand, sorry");
+        }
+        return instance;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    @Override
+    public boolean equals(Object o)
+    {
+        if (this == o)
+        {
+            return true;
+        }
+
+        if (o == null || getClass() != o.getClass())
+        {
+            return false;
+        }
+
+        Car car = (Car) o;
+
+        return new EqualsBuilder()
+                .append(name, car.name)
+                .isEquals();
+    }
+
+    @Override
+    public int hashCode()
+    {
+        return new HashCodeBuilder(17, 37)
+                .append(name)
+                .toHashCode();
+    }
+}
---
+++ src/test/java/com/public_class/snippets/staticFactoryMethod/CarTest.java
@@ -0,0 +1,39 @@
+package com.public_class.snippets.staticFactoryMethod;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import static com.public_class.snippets.staticFactoryMethod.Car.of;
+import static org.junit.Assert.assertEquals;
+
+@RunWith(MockitoJUnitRunner.class)
+public class CarTest
+{
+    private static final String BMW = "BMW";
+
+    private static final Car BMW_CAR = of(BMW);
+
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldThrowIllegalArgumentExceptionWhenBrandIsNotCached()
+    {
+        // given
+        final String brand = "Audi";
+
+        // when
+        final Car result = of(brand);
+    }
+
+    @Test
+    public void shouldReturnInstanceOfBwmWhenPassingMixedCaseBmwString()
+    {
+        // given
+        final String mixedCaseBmw = "BmW";
+
+        // when
+        final Car result = of(mixedCaseBmw);
+
+        // then
+        assertEquals(BMW_CAR, result);
+    }
+}
