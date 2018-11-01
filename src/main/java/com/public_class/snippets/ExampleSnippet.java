988c702d94f5e4e602d1e4e54dc77727ca7f5806
Jakub Dubański <jakub.dubanski@gmail.com>
2018-11-01 22:26:11 +0100

    [Snippets] Correct package.

--- src/main/java/com/public_class/snippets/ExampleSnippet.java
+++ src/main/java/com/public_class/snippets/ExampleSnippet.java
@@ -9,7 +9,8 @@ public class ExampleSnippet
     public static void main(String[] args)
     {
         final Mother mother = new Mother();
-        final Mother.CheerfulMother cheerful = mother.new CheerfulMother(); // take a note about this guy
+        final Mother.CheerfulMother cheerful = mother.new CheerfulMother();
+        // take a note about this guy
 
         final ExecutorService service = newCachedThreadPool();
         service.submit(cheerful::sayHi);
