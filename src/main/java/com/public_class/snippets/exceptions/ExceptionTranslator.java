f984f0d294e8389fd51bc0abb5928637b4fca495
Jakub Dubański <jakub.dubanski@gmail.com>
2018-12-15 17:15:11 +0100

    [Snippets] How simple is to translate exceptions?

---
+++ src/main/java/com/public_class/snippets/exceptions/ExceptionTranslator.java
@@ -0,0 +1,22 @@
+package com.public_class.snippets.exceptions;
+
+// Kubanino @ https://public-class.com/
+public class ExceptionTranslator
+{
+    private static IllegalStateException translateToIllegalStateException(Throwable exception)
+    {
+        return new IllegalStateException(exception);
+    }
+
+    @SuppressWarnings("ConstantConditions")
+    public static void main(String[] args)
+    {
+        Throwable exception = new NullPointerException("Well, I am one of a kind. Am I?");
+        Throwable translated = translateToIllegalStateException(exception);
+
+        System.out.println("Do not repeat such crazy code at home!");
+        System.out.println(translated.getCause().getMessage());
+        System.out.println(translated instanceof NullPointerException);
+        System.out.println(translated instanceof IllegalStateException);
+    }
+}
