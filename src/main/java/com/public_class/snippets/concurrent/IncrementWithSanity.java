4a428e7226878485c76f8ef81d8ce1db12b2535b
Jakub Dubański <jakub.dubanski@gmail.com>
2018-12-16 16:17:53 +0100

    [Snippets] Synchronize with sanity #2.

--- src/main/java/com/public_class/snippets/concurrent/IncrementWithSanity.java
+++ src/main/java/com/public_class/snippets/concurrent/IncrementWithSanity.java
@@ -2,6 +2,7 @@ package com.public_class.snippets.concurrent;
 
 import static java.lang.Thread.currentThread;
 
+// Kubanino @ https://public-class.com/
 public class IncrementWithSanity
 {
     private static int VALUE = 0;
