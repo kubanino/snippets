d866a4a59983cb7d6141423fd5d5e86b937b7bde
Jakub Dubański <jakub.dubanski@gmail.com>
2019-02-06 20:19:19 +0100

    [Snippets] Simple queue fun.

---
+++ src/main/java/com/public_class/snippets/collections_usage/QueueFun.java
@@ -0,0 +1,40 @@
+package com.public_class.snippets.collections_usage;
+
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.Random;
+
+import static java.lang.System.out;
+
+public class QueueFun
+{
+    private static final Random RANDOM = new Random();
+
+    public static void main(String[] args)
+    {
+        Queue<String> queue = new PriorityQueue<>();
+
+        for (int i = 0; i < 10; i++)
+        {
+            final String generatedString = new String(new char[]{(char) (75 - RANDOM.nextInt(10))});
+            out.println("Generated string: " + generatedString);
+            queue.add(generatedString);
+        }
+        out.println("Priority queue will use natural comparison");
+        queue.forEach(out::println);
+
+        out.println("Let's peek the first element, twice");
+        out.println(queue.peek());
+        out.println(queue.peek());
+
+        out.println("Let's pool the first element 3 times");
+        out.println(queue.poll());
+        out.println(queue.poll());
+        out.println(queue.poll());
+
+        out.println("Ok, so does offer work?");
+        out.println(queue.offer("Hello Man"));
+
+
+    }
+}
