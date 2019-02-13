361de37191e97031037bec2b2b6819caef41aa75
Jakub Dubański <jakub.dubanski@gmail.com>
2019-02-13 19:59:11 +0100

    [Snippets] Start of parallel exercise.

---
+++ src/main/java/com/public_class/snippets/collections_usage/ParallelismOfAggregateOperations.java
@@ -0,0 +1,11 @@
+package com.public_class.snippets.collections_usage;
+
+// Kubanino @ https://public-class.com/
+public class ParallelismOfAggregateOperations
+{
+    public static void main(String[] args)
+    {
+
+
+    }
+}
