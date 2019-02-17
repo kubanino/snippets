52edb1c5d73f8f11fc1bd979dafcbfe271561bf8
Jakub Dubański <jakub.dubanski@gmail.com>
2019-02-17 12:33:33 +0100

    [Snippets] Stack vs queue.

---
+++ src/main/java/com/public_class/snippets/collections_usage/StackVsQueue.java
@@ -0,0 +1,40 @@
+package com.public_class.snippets.collections_usage;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
+// Kubanino @ https://public-class.com/
+public class StackVsQueue
+{
+    public static void main(String[] args)
+    {
+        Stack<Integer> stack = new Stack<>();
+        Queue<Integer> queue = new LinkedList<>();
+
+        // populate
+        for (int i = 0; i < 10; i++)
+        {
+            stack.push(i);
+            queue.offer(i);
+        }
+        // peek does nothing on both
+        System.out.println("PEEK DOES NOTHING ON BOTH:");
+        for (int i = 0; i < 10; i++)
+        {
+            System.out.println(stack.peek());
+            System.out.println(stack.peek());
+        }
+        // read
+        System.out.println("STACK:");
+        for (int i = 0; i < 10; i++)
+        {
+            System.out.println(stack.pop());
+        }
+        System.out.println("QUEUE:");
+        for (int i = 0; i < 10; i++)
+        {
+            System.out.println(queue.poll());
+        }
+    }
+}
