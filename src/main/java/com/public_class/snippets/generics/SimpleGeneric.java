804761d693a93027c9e088e2d0e1ece9f120a9a0
Jakub Dubański <jakub.dubanski@gmail.com>
2019-02-18 17:58:23 +0100

    [Snippets] Simple generic class.

---
+++ src/main/java/com/public_class/snippets/generics/SimpleGeneric.java
@@ -0,0 +1,59 @@
+package com.public_class.snippets.generics;
+
+import java.util.Objects;
+
+/**
+ * @param <T> E - Element (used extensively by the Java Collections Framework)
+ *            K - Key
+ *            N - Number
+ *            T - Type
+ *            V - Value
+ *            S,U,V etc. - 2nd, 3rd, 4th types
+ */
+
+
+// Kubanino @ https://public-class.com/
+public class SimpleGeneric<T, S>
+{
+    private final T innerObject;
+    private final S anotherInnerObject;
+
+    public SimpleGeneric(T innerObject, S anotherInnerObject)
+    {
+        this.innerObject = innerObject;
+        this.anotherInnerObject = anotherInnerObject;
+    }
+
+    public T getInnerObject()
+    {
+        return innerObject;
+    }
+
+    public S getAnotherInnerObject()
+    {
+        return anotherInnerObject;
+    }
+
+    @Override
+    public boolean equals(Object o)
+    {
+        if (this == o)
+        {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass())
+        {
+            return false;
+        }
+        SimpleGeneric<?, ?> that = (SimpleGeneric<?, ?>) o;
+        return Objects.equals(innerObject, that.innerObject) &&
+                Objects.equals(anotherInnerObject, that.anotherInnerObject);
+    }
+
+    @Override
+    public int hashCode()
+    {
+
+        return Objects.hash(innerObject, anotherInnerObject);
+    }
+}
