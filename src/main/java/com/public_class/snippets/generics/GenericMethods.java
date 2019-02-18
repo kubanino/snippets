9e8fad41d2203b28442f54500b37fcdd55759920
Jakub Dubański <jakub.dubanski@gmail.com>
2019-02-18 18:22:22 +0100

    [Snippets] Generic methods example.

---
+++ src/main/java/com/public_class/snippets/generics/GenericMethods.java
@@ -0,0 +1,23 @@
+package com.public_class.snippets.generics;
+
+// Kubanino @ https://public-class.com/
+@SuppressWarnings({"WeakerAccess", "RedundantTypeArguments"})
+public class GenericMethods
+{
+    public static <T extends Number, S extends Number> double addThemUp(T first, S second)
+    {
+        return first.doubleValue() + second.doubleValue();
+    }
+
+    public static void main(String[] args)
+    {
+        Long cash = 1L;
+        Float equities = 3F;
+
+        // with full syntax, the invoke looks like that:
+        System.out.println(GenericMethods.<Long, Float>addThemUp(cash, equities));
+
+        // You do not actually need some parts of it, as compiler will deduce the type
+        System.out.println(addThemUp(cash, equities));
+    }
+}
