df7c922508eb60e1ce11789f7efccf09a2e3aba3
Jakub Dubański <jakub.dubanski@gmail.com>
2019-02-19 17:32:35 +0100

    [Snippets] Type capture examample.

---
+++ src/main/java/com/public_class/snippets/generics/WildcardTypeCaptureExample.java
@@ -0,0 +1,23 @@
+package com.public_class.snippets.generics;
+
+import java.util.List;
+
+// Kubanino @ https://public-class.com/
+public class WildcardTypeCaptureExample
+{
+    // the problem is, that list is declared as List<?> and You can not put anything there
+    public static void resetFirstValue(List<?> list)
+    {
+        // list.set(0, list.get(0));
+    }
+
+    public static void resetFirstValueWithTypeCapture(List<?> list)
+    {
+        resetFirstValueWithTypeCaptureHelper(list);
+    }
+
+    private static <T> void resetFirstValueWithTypeCaptureHelper(List<T> list)
+    {
+        list.set(0, list.get(0));
+    }
+}
