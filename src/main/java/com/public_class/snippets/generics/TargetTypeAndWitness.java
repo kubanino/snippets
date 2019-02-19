d50f56db013c62868fbbf24c445ef48976ebdac2
Jakub Dubański <jakub.dubanski@gmail.com>
2019-02-19 17:12:25 +0100

    [Snippets] Target Type and Witness.

---
+++ src/main/java/com/public_class/snippets/generics/TargetTypeAndWitness.java
@@ -0,0 +1,28 @@
+package com.public_class.snippets.generics;
+
+import java.util.Collections;
+import java.util.List;
+
+// Kubanino @ https://public-class.com/
+@SuppressWarnings({"WeakerAccess", "RedundantTypeArguments"})
+public class TargetTypeAndWitness
+{
+    /**
+     * public static final <T> List<T> emptyList() {
+     * return (List<T>) EMPTY_LIST;
+     * }
+     */
+
+    public static void main(String[] args)
+    {
+        // note here, compiler deduced the type T is String not a List<String>
+        List<String> list = Collections.emptyList();
+
+        // You can also use redundant type Witness
+        List<String> anotherList = Collections.<String>emptyList(); // T is always before name of method
+
+        // it works only from Java8 in the context of method arguments, i.e
+        // foo(Collections.emptyList()) will not work in Java <8 if foo(List<String> list)
+        // compiler was not ready to infer type from method arguments
+    }
+}
