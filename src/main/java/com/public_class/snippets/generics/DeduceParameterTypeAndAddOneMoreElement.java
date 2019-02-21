bb286a12855e4f095d57760501cc55919a34f998
Jakub Dubański <jakub.dubanski@gmail.com>
2019-02-21 19:42:39 +0100

    [Snippets] Small type erasure example.

---
+++ src/main/java/com/public_class/snippets/generics/DeduceParameterTypeAndAddOneMoreElement.java
@@ -0,0 +1,27 @@
+package com.public_class.snippets.generics;
+
+import java.util.ArrayList;
+import java.util.List;
+
+// Kubanino @ https://public-class.com/
+public class DeduceParameterTypeAndAddOneMoreElement
+{
+    public static void main(String[] args)
+    {
+        List<Double> listOfDoubles = new ArrayList<>();
+        listOfDoubles.add(1D);
+
+        addOneMoreNumberWithValueOne(listOfDoubles);
+    }
+
+    public static void addOneMoreNumberWithValueOne(List<?> listOfNumbers)
+    {
+        addOneMoreNumberWithValueOneHelper(listOfNumbers);
+    }
+
+    private static <T> void addOneMoreNumberWithValueOneHelper(List<T> listOfNumbers)
+    {
+       // You would expect reflection things to get type of that collection, but You can not - type erasure in action
+    }
+}
+
