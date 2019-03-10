86d81197cd49bd521ddc2b7a41a208622138afec
Jakub Dubański <jakub.dubanski@gmail.com>
2019-03-10 19:56:13 +0100

    [Snippets] SocketChannelExample.

--- src/main/java/com/public_class/snippets/nio/SocketChannelExample.java
+++ src/main/java/com/public_class/snippets/nio/SocketChannelExample.java
@@ -1,6 +1,9 @@
 package com.public_class.snippets.nio;
 
 import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SocketChannel;
 
 // http://tutorials.jenkov.com/java-nio/channels.html
 // // Kubanino @ https://public-class.com/
@@ -8,6 +11,20 @@ public class SocketChannelExample
 {
     public static void main(String[] args) throws IOException // for some convenience this exception is here
     {
+        SocketChannel socketChannel = SocketChannel.open();
+        socketChannel.connect(new InetSocketAddress("https://www.google.pl/", 80));
 
+        ByteBuffer buffer = ByteBuffer.allocate(48);
+        int bytesRead = socketChannel.read(buffer);
+
+        while (bytesRead != -1)
+        {
+            buffer.flip();
+            while (buffer.hasRemaining())
+            {
+                System.out.print(buffer.get());
+            }
+            bytesRead = socketChannel.read(buffer);
+        }
     }
 }
